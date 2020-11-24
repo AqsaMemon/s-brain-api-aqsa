514a4a7022e46b97d9f4f31ef869e5043c4bbd57
AqsaMemon <aqsamemon@hotmail.com>
2020-11-24 14:46:58 -0800

    server

---
+++ smart-brain-api-aqsa/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/signin');
+const image = require('./controllers/image');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host : '127.0.0.1',
+		user : 'aqsamemon',
+		password : '',
+		database : 'smart-brainer'
+	}
+});
+
+// db.select('*').from('users').then(data => {
+// 	console.log(data);
+// });
+
+const app = express();
+// const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// const database = {
+// 	users: [
+// 	{		
+// 		id: '123',
+// 		name: 'John',
+// 		password: 'cookies',
+// 		email: 'john@gmail.com',
+// 		entries: 0,
+// 		joined: new Date()
+// 	},
+// 	{
+// 		id: '124',
+// 		name: 'Sally',
+// 		password: 'bananas',
+// 		email: 'sally@gmail.com',
+// 		entries: 0,
+// 		joined: new Date()
+// 	}
+// 	],
+// 	login: [
+// 	{
+// 		id: '987',
+// 		hash:'',
+// 		email: 'john@gmail.com'
+// 	}
+// 	]
+// }
+
+app.get('/', (req, res)=> { res.send(database.users) })
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt)})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
+
+app.listen(3000,() => {
+	console.log('app is running on port 3000');
+})
